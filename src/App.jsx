607fc690c938db93303d917ecde125e23a9e5d16
Marcos Vinicius <marcosvinicios4132@gmail.com>
2023-08-28 11:24:12 -0300

    memo aplicado no projeto

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,18 @@ import { Header } from './Header';
 
 
 function App() {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [username, setUsername] = useState("")
-  const [description, setDescription] = useState("")
-  const [type, setType] = useState("user")
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
   function handleSave(e){
     e.preventDefault();
-
-    console.log({
-      name,
-      email,
-      username,
-      description,
-      type,
-    })
   }
 
 
   return (
     <div className="container">
       <h1>React</h1>
-      <Header/>
+      <Header name={name}/>
 
       <form className="form" onSubmit={handleSave}>
         <input
@@ -44,33 +33,6 @@ function App() {
           placeholder="Digite seu email..."
           className="input"
         />
-
-        <input
-          type="text"
-          value={username}
-          onChange={ (event) => setUsername(event.target.value) }
-          placeholder="Digite seu username..."
-          className="input"
-        />
-
-        <textarea
-          type="text"
-          value={description}
-          onChange={ (event) => setDescription(event.target.value) }
-          placeholder="Digite sua descriçao..."
-          className="input"
-        ></textarea>
-
-
-        <select  
-          className="select"
-          value={type}
-          onChange={e => setType(e.target.value)}
-        >
-          <option value="user">user</option>
-          <option value="admin">admin</option>
-        </select>
-
 
         <button className="button" type="submit">Enviar</button>
       </form>
--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,11 +1,19 @@
+import { memo } from "react"; //Utilizando para criar componentes memorizados
 
-let renderCount = 0;
+/**
+ * Exportamos o componente memorizado
+ * 
+ * Quando criamos um componente memorizado no react, 
+ * ele apenas será renderizado quando os estados 
+ * dele sofrerem alteração, caso contrário, 
+ * ele não renderiza o componente novamente
+ */
+export const Header = memo(({ name }) => {
+    console.log('componente header renderizou')
+    return (
+     <h3>Bem-vindo {name}</h3>
+    )
+});
 
-export function Header() {
-  renderCount++;
-  
-  return (
-   <h3>renderizou {renderCount}</h3>
-  )
-}
+
 
